654d5942ca509886b9970d6748132109270f398c
Sergio Di Pietro <usr2264@salesianos-lacuesta.net>
2021-09-23 13:59:09 +0100

    Commit inicial. Creadas la estructura básica del proyecto y la clase principal.

---
+++ src/UD1_1.java
@@ -0,0 +1,14 @@
+/*Adjunto a este enunciado se te facilitará un fichero de texto que contendrá un registro por línea.
+Dentro de este fichero se encuentran nombres de personas ficticias.
+De este fichero deberás seleccionar diez nombres de manera aleatoria, es decir, no debes seleccionar
+        los diez primeros sino que en cada ejecución de tu programa deberá aparecer una lista de personas distinta.
+Para cada una de estas personas ficticias deberás inventarte una lista de cuatro calificaciones aleatorias entre
+        0 y 10. Las calificaciones podrán tener decimales. Con esas calificaciones tendrás que calcular la media de cada alumno.
+El cálculo de la media consiste en sumar las cuatro calificaciones y dividirlas entre cuatro.
+Para finalizar, tendrás que mostrar por pantalla una fila por cada estudiante con sus cuatro calificaciones y su media.
+Esta misma información tendrás que guardarla en un fichero en el mismo directorio donde se encuentre el ejecutable del programa.*/
+
+public class UD1_1 {
+    public static void main(String[] args) {
+    }
+}
