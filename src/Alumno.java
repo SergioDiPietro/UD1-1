862d407fc45a7b02906a40da785e2c87aacd873d
Sergio Di Pietro <usr2264@salesianos-lacuesta.net>
2021-09-28 20:06:28 +0100

    Funcionalidad implementada: ahora se muestran por pantalla los alumnos con sus notas y sus medias, además de la generación de un fichero con esta misma información.

--- src/Alumno.java
+++ src/Alumno.java
@@ -1,6 +1,7 @@
 public class Alumno {
 
     // Atributos
+
     private String nombre;
     private float nota1;
     private float nota2;
@@ -9,6 +10,7 @@ public class Alumno {
     private float media;
 
     // Constructores
+
     public Alumno() {
         this.nombre = "";
         this.nota1 = 0;
@@ -17,12 +19,63 @@ public class Alumno {
         this.nota4 = 0;
         this.media = 0;
     }
+
     public Alumno(String nombre) {
         this.nombre = nombre;
         this.nota1 = (float)Math.random()*10;
-        this.nota1 = (float)Math.random()*10;
-        this.nota1 = (float)Math.random()*10;
-        this.nota1 = (float)Math.random()*10;
+        this.nota2 = (float)Math.random()*10;
+        this.nota3 = (float)Math.random()*10;
+        this.nota4 = (float)Math.random()*10;
         this.media = (nota1+nota2+nota3+nota4)/4;
+    }
+
+    // Getters & Setters
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public float getNota1() {
+        return nota1;
+    }
+
+    public void setNota1(float nota1) {
+        this.nota1 = nota1;
+    }
+
+    public float getNota2() {
+        return nota2;
+    }
+
+    public void setNota2(float nota2) {
+        this.nota2 = nota2;
+    }
+
+    public float getNota3() {
+        return nota3;
+    }
+
+    public void setNota3(float nota3) {
+        this.nota3 = nota3;
+    }
+
+    public float getNota4() {
+        return nota4;
+    }
+
+    public void setNota4(float nota4) {
+        this.nota4 = nota4;
+    }
+
+    public float getMedia() {
+        return media;
+    }
+
+    public void setMedia(float media) {
+        this.media = media;
     }
 }
--- src/UD1_1.java
+++ src/UD1_1.java
@@ -8,21 +8,61 @@ El cálculo de la media consiste en sumar las cuatro calificaciones y dividirlas
 Para finalizar, tendrás que mostrar por pantalla una fila por cada estudiante con sus cuatro calificaciones y su media.
 Esta misma información tendrás que guardarla en un fichero en el mismo directorio donde se encuentre el ejecutable del programa.*/
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.*;
 import java.util.ArrayList;
 
 public class UD1_1 {
     public static void main(String[] args) {
-        final String RUTA = "C:\\Users\\Sergio\\Clases\\2DAM\\Programacion Multimedia\\Tema 1\\UD1-1\\src\\Listado.csv";
+        final String RUTA_ENTRADA = "C:\\Users\\Sergio\\Clases\\2DAM\\Programacion Multimedia\\Tema 1\\UD1-1\\src\\Listado.csv";
+        final String RUTA_SALIDA = "C:\\Users\\Sergio\\Clases\\2DAM\\Programacion Multimedia\\Tema 1\\UD1-1\\src\\ListadoFinal.csv";
         final short CANTIDAD_ALUMNOS = 10;
 
-        ArrayList<String> arrAlumnos = listToArrayList(RUTA);
-        ArrayList<String> arrFinal = listPicks(arrAlumnos, CANTIDAD_ALUMNOS);
+        ArrayList<String> arrAlumnos = listPicks(listToArrayList(RUTA_ENTRADA), CANTIDAD_ALUMNOS);
+
+        File fichero = new File(RUTA_SALIDA);
+        PrintWriter pw = null;
+
+        if (fichero.exists()) fichero.delete();
+
+        try {
+            fichero.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            pw = new PrintWriter(RUTA_SALIDA);
+            for (int i=0; i<(CANTIDAD_ALUMNOS); i++) {
+                Alumno alumno = new Alumno(arrAlumnos.get(i));
+
+                String nota1Formateada = String.format("%.02f", alumno.getNota1());
+                String nota2Formateada = String.format("%.02f", alumno.getNota2());
+                String nota3Formateada = String.format("%.02f", alumno.getNota3());
+                String nota4Formateada = String.format("%.02f", alumno.getNota4());
+                String mediaFormateada = String.format("%.02f", alumno.getMedia());
+
+                String linea = "Alumno/a: " + alumno.getNombre() +
+                        "; Notas: " + nota1Formateada + ", " + nota2Formateada + ", "
+                        + nota3Formateada + ", " + nota4Formateada + "; Media: " + mediaFormateada;
+
+                System.out.println(linea);
+                pw.println(linea);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (pw != null)
+                    pw.close();
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
     }
 
+    //Esta función recibe como parámetro un String correspondiente a una ruta y devuelve un ArrayList de String
+    // con los elementos que contenga ese archivo.
     public static ArrayList<String> listToArrayList(String ruta) {
-        FileReader fr = null;
         BufferedReader br = null;
         ArrayList<String> arrNombres = new ArrayList<>();
 
@@ -49,6 +89,8 @@ public class UD1_1 {
         return arrNombres;
     }
 
+    //Esta función recibe como parámetro un ArrayList de String y devuelve otro array pero sacando un número
+    // establecido de elementos aleatorios de dicha lista.
     public static ArrayList<String> listPicks(ArrayList<String> arr, int picks) {
         ArrayList<String> arrFinal = new ArrayList<>();
         for (int i=0; i<picks; i++) {
